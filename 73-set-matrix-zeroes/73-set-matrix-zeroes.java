7d7eab3bdc9da82ceb66864bf09893e52acb70ef
Alwin John <47777599+Dexters-Hub@users.noreply.github.com>
2022-04-23 22:33:45 +0530

    Time: 1 ms (88.07%), Space: 43.8 MB (84.93%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean[] zeroRow = new boolean[matrix.length];
+        boolean[] zeroColumn = new boolean[matrix[0].length];
+        for (int i = 0 ; i < matrix.length ;i++){
+            for(int j = 0 ; j < matrix[0].length ; j++){
+                if(matrix[i][j] == 0){
+                    zeroRow[i] = true;
+                    zeroColumn[j] = true;
+                }
+            }
+        }
+        for (int i = 0 ; i < matrix.length ;i++){
+            for(int j = 0 ; j < matrix[0].length ; j++){
+                if(zeroRow[i] || zeroColumn[j]){
+                    matrix[i][j] =0;
+                }
+            }
+        }
+    }
+    
+}
