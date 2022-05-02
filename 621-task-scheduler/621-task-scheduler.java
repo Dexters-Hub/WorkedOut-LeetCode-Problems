0e1c1960ab15246a4eec129fb2f24c6df392b503
Alwin John <47777599+Dexters-Hub@users.noreply.github.com>
2022-05-02 23:06:00 +0530

    Time: 2 ms (98.66%), Space: 43.2 MB (88.30%) - LeetHub

---
+++ 621-task-scheduler/621-task-scheduler.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        
+        int[] counter = new int[26];
+        
+        for (char task : tasks) {
+            counter[task - 'A']++;
+        }
+        
+        Arrays.sort(counter);
+        int maxFrequency = counter[25];
+        int idleTime = (maxFrequency - 1) * n;
+        
+        for (int i = counter.length - 2; i >= 0 && idleTime > 0; i--) {
+        
+            idleTime -= Math.min(maxFrequency - 1, counter[i]);
+        }
+        
+        idleTime = Math.max(0, idleTime);
+        
+        return idleTime + tasks.length;
+    }
+}
