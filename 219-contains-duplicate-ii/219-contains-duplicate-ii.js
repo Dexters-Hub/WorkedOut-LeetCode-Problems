6e95f48e752801383581d50fbe642ccc9cf97614
Alwin John <47777599+Dexters-Hub@users.noreply.github.com>
2022-08-25 20:13:23 +0530

    Time: 110 ms (95.80%), Space: 65.4 MB (42.60%) - LeetHub

---
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    const map = new Map()
+    
+    for(let i=0;i<nums.length; i++){
+        if(map.has(nums[i])){
+            const j= map.get(nums[i])
+            
+            if(Math.abs(i-j)<=k){
+                return true
+            }
+        }
+        
+        map.set(nums[i],i)
+    }
+    
+    return false
+};
